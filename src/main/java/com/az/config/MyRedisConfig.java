799f409a29b0e7c86ce844baa3d4bf62bc36b18e
armin <arminzheng@qq.com>
2023-06-25 09:49:51 +0800

    fix

--- src/main/java/com/az/config/MyRedisConfig.java
+++ src/main/java/com/az/config/MyRedisConfig.java
@@ -33,13 +33,11 @@ public class MyRedisConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
 
-        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                                                                            .entryTtl(Duration.ofDays(
-                                                                                    1))
-                                                                            .disableCachingNullValues()
-                                                                            .serializeValuesWith(
-                                                                                    RedisSerializationContext.SerializationPair.fromSerializer(
-                                                                                            new GenericJackson2JsonRedisSerializer()));
+        RedisCacheConfiguration cacheConfiguration =
+                RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(1))
+                                       .disableCachingNullValues()
+                                       .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                                               new GenericJackson2JsonRedisSerializer()));
         return RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
     }
     //
